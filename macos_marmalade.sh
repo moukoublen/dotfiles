20096bb85c89e0ca7ac7ebeb40b15729f4e95dbb
Kostas Stamatakis <kostas@baconstrip.local>
2016-10-10 13:36:56 +0300

    mac stuff

--- macos_marmalade.sh
+++ macos_marmalade.sh
@@ -5,15 +5,18 @@ if [ -f $(brew --prefix)/etc/bash_completion ]; then
 fi
 ####################################################################
 ####################################################################
+export LANG='en_US.UTF-8';
+export LC_ALL='en_US.UTF-8';
+
+add_to_start_of_path_if_not_exists $HOME/bin
+
+# brew install coreutils (e.g.: cat, chmod, chroot, cp, dd, dir, du, echo, ls ... etc)
+add_to_start_of_path_if_not_exists /usr/local/opt/coreutils/libexec/gnubin
+
+# brew install findutils (find, locate, updatedb, xargs)
+add_to_start_of_path_if_not_exists /usr/local/opt/findutils/libexec/gnubin
 
 alias cleanupFinderShit="find . -type f -name '*.DS_Store' -ls -delete"
 alias ldd='otool -L'
-alias ll='ls -la -G'
+alias ll='command ls -la --color=auto'
 alias grep='grep --color=auto'
-
-add_to_start_of_path_if_not_exists $HOME/bin
-add_to_start_of_path_if_not_exists /usr/local/opt/coreutils/libexec/gnubin
-add_to_start_of_path_if_not_exists /usr/local/opt/findutils/libexec/gnubin
-
-export LANG='en_US.UTF-8';
-export LC_ALL='en_US.UTF-8';
