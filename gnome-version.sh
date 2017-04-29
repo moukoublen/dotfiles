3a6ab57e057873514100eb7727c4dc6228aceaa2
Kostas Stamatakis <moukoublen@gmail.com>
2017-04-29 13:24:21 +0300

    update gnome version script

--- gnome-version.sh
+++ gnome-version.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
 
 function print-version-if-exists {
-  if which $1 > /dev/null 2>&1; then $1 ${2:---version}; fi
+  if which $1 > /dev/null 2>&1; then echo "$1 $($1 ${2:---version} | sed 's/[^0-9]*//')" ; fi
 }
 
-if which nautilus > /dev/null 2>&1; then nautilus --version |& grep GNOME; fi
+function print-version-of-gnome-apps {
+  if which nautilus > /dev/null 2>&1; then echo "nautilus $(nautilus --version |& grep GNOME | sed 's/[^0-9]*//')"; fi
 
-for prb in \
-gnome-shell \
-gnome-terminal \
-gedit \
-gnome-builder \
-gnome-calculator \
-gnome-calendar \
-gnome-control-center \
-gnome-software \
-gnome-books \
-gnome-calculator \
-gnome-system-monitor
-do
-  print-version-if-exists $prb
-done
+  for prb in \
+  gnome-shell \
+  gnome-terminal \
+  gedit \
+  gnome-builder \
+  gnome-calendar \
+  gnome-control-center \
+  gnome-software \
+  gnome-books \
+  gnome-system-monitor
+  do
+    print-version-if-exists $prb | column -t
+  done
+}
+
+
+print-version-of-gnome-apps | column -t
