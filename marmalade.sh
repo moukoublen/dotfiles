61d950a55ba244d1da9fc3c4d14b5f697692a83f
Kostas Stamatakis <moukoublen@gmail.com>
2017-07-22 13:36:51 +0300

    optional go

--- marmalade.sh
+++ marmalade.sh
@@ -50,11 +50,14 @@ alias update_marmalade="(cd ${MARMALADE_PATH}; git pull)"
 
 ################################################################################
 ######### Go Language ##########################################################
-# No need of GOROOT in case of default path (/usr/local/go)
-#export GOROOT=/path/to/special/go
-add_to_start_of_path_if_not_exists /usr/local/go/bin
-export GOPATH=$HOME/goworkspace
-alias set_gopath_bin_to_path='export PATH=$PATH:$GOPATH/bin'
+if [ -d /usr/local/go ]
+then
+  # No need of GOROOT in case of default path (/usr/local/go)
+  #export GOROOT=/path/to/special/go
+  add_to_start_of_path_if_not_exists /usr/local/go/bin
+  export GOPATH=$HOME/goworkspace
+  alias set_gopath_bin_to_path='export PATH=$PATH:$GOPATH/bin'
+fi
 ################################################################################
 ################################################################################
 
