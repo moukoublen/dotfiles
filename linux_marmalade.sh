341458d0028d26d15c8e5d0433b6ca5cc8442e32
Kostas Stamatakis <moukoublen@gmail.com>
2017-09-09 21:00:18 +0300

    add get_node function

--- linux_marmalade.sh
+++ linux_marmalade.sh
@@ -87,6 +87,11 @@ add_to_end_of_path_if_not_exists $GROOVY_HOME/bin
 export NODE_HOME=/opt/node/default
 add_to_end_of_path_if_not_exists $NODE_HOME/bin
 #if which npm > /dev/null 2>&1; then source <(npm completion); fi
+get_node() {
+  #$1 : node version e.g.  "8.4.0"
+  curl -L https://nodejs.org/dist/v$1/node-v$1-linux-x64.tar.xz > /tmp/node.tar.xz
+  sudo tar xf /tmp/node.tar.xz -C /opt/node/
+}
 ################################################################################
 ################################################################################
 
