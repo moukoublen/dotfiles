51a9e9a86c3ea2e16e9150072cc55e0237e6d9d1
Kostas Stamatakis <moukoublen@gmail.com>
2017-09-25 22:55:11 +0300

    atom env

---
+++ envs/atom_env.sh
@@ -0,0 +1,18 @@
+install_my_atom_packages() {
+  apm install \
+    atom-beautify \
+    file-icons \
+    highlight-selected \
+    language-scala \
+    linter \
+    linter-ui-default \
+    intentions \
+    busy-signal \
+
+  #themes
+  # nord-atom-syntax
+  # nord-atom-ui
+
+  # nova-atom-syntax
+  # nova-atom-ui
+}
