674b7d69148cc15bb4a9886b84d61831be59a560
Kostas Stamatakis <moukoublen@gmail.com>
2017-10-04 19:47:01 +0300

    go

--- envs/go_env.sh
+++ envs/go_env.sh
@@ -7,3 +7,22 @@ then
   alias set_gopath_bin_to_path='export PATH=$PATH:$GOPATH/bin'
 fi
 
+################################################################################
+# Marmalade Env Management
+################################################################################
+_marmalade_get_latest_version_number__go() {
+  go version | sed 's/go version//' | sed 's/go//' | sed 's/ linux\/amd64//'
+}
+
+_marmalade_get_local_latest_version_number__go() {
+  go version | sed 's/go version//' | sed 's/go//' | sed 's/ linux\/amd64//'
+}
+
+_marmalade_install__go() {
+  # $1: version e.g. 1.9
+  # https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
+  curl -L https://storage.googleapis.com/golang/go$1.linux-amd64.tar.gz > go$1.linux-amd64.tar.gz
+  if [ -d /usr/local/go ]; then sudo rm -rf /usr/local/go; fi
+  sudo tar -C /usr/local -xzf go$1.linux-amd64.tar.gz
+  rm /tmp/go$1.linux-amd64.tar.gz
+}
