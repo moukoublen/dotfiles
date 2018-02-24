39102cd054a5e80b0891bbc77186bc612e20e81a
Kostas Stamatakis <moukoublen@gmail.com>
2018-02-24 19:07:37 +0200

    ps1

--- marmalade_ps1.sh
+++ marmalade_ps1.sh
@@ -13,23 +13,23 @@ export GIT_PS1_SHOWCOLORHINTS=true
 _marmalade_ps1() {
   [[ -z $BASH ]] && return
 
-  local F_SEP='\[\033[2;49;37m\]'
+  local F_SEP='\[\033[2;37m\]'
   local F_PATH='\[\033[32m\]'
-  local F_GIT='\[\033[93;1m\]'
+  local F_GIT='\[\033[1;93m\]'
   local F_MAIN='\[\033[36m\]'
   local RESET_ALL='\[\033[0m\]'
 
-  local p_s1="${F_SEP}╭${RESET_ALL}"
-  local p_s2="${F_SEP}╰${RESET_ALL}"
-  local p_sep="${RESET_ALL}${F_SEP}|${RESET_ALL}"
-  local p_git="${F_GIT}\$(declare -F __git_ps1 &>/dev/null && __git_ps1 \"%s\")${RESET_ALL}"
+  local p_s1="${F_MAIN}╭${RESET_ALL}"
+  local p_s2="${F_MAIN}╰${RESET_ALL}"
+  local p_sep="${F_SEP}|${RESET_ALL}"
+  local p_git="\$(declare -F __git_ps1 &>/dev/null && __git_ps1 \"${p_sep} ${F_GIT}%s${RESET_ALL}\")"
   local p_pwd="${F_PATH}\w${RESET_ALL}"
   local p_usr="${F_MAIN}\u@\h${RESET_ALL}"
 
-  local line1="${p_s1} ${p_usr} ${p_sep} ${p_pwd} ${p_sep} ${p_git}"
-  local line2="${p_s2} "
+  local ps1_line1="${p_s1} ${p_usr} ${p_sep} ${p_pwd} ${p_git}"
+  local ps1_line2="${p_s2} "
 
-  export PS1="${line1}\n${line2}"
+  export PS1="${ps1_line1}\n${ps1_line2}"
 }
 
 _marmalade_ps1
