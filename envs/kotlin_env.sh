18563d43cf120f09d75222cf593de54978b44ce1
Kostas Stamatakis <moukoublen@gmail.com>
2018-04-02 23:36:24 +0300

    update kotlin_env.sh

--- envs/kotlin_env.sh
+++ envs/kotlin_env.sh
@@ -4,7 +4,7 @@ add_to_end_of_path_if_not_exists $MARMALADE_ENVS/kotlin/default/bin
 # Marmalade Env Management
 ################################################################################
 _marmalade_get_latest_version_number__kotlin() {
-  curl -s https://api.github.com/repos/JetBrains/kotlin/releases/latest | jq -r '.name'
+  curl -s https://api.github.com/repos/JetBrains/kotlin/releases/latest | jq -r '.tag_name' | tr -d 'v'
 }
 
 _marmalade_get_local_latest_version_number__kotlin() {
@@ -15,7 +15,9 @@ _marmalade_install__kotlin() {
   # $1: version e.g. 4.1
   # https://github.com/JetBrains/kotlin/releases/download/v1.1.51/kotlin-compiler-1.1.51.zip
   #
-  curl -L --fail https://github.com/JetBrains/kotlin/releases/download/v$1/kotlin-compiler-$1.zip > /tmp/kotlin.zip
+  local url="https://github.com/JetBrains/kotlin/releases/download/v$1/kotlin-compiler-$1.zip"
+  echo "    Download $url"
+  curl -L --fail $url > /tmp/kotlin.zip
   mkdir -p $MARMALADE_ENVS/kotlin/$1
   unzip -qq /tmp/kotlin.zip -d /tmp/
   mv /tmp/kotlinc/* $MARMALADE_ENVS/kotlin/$1
