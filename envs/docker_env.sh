9da91e57806ae848e5df98c846c294b0b3af07b8
Kostas Stamatakis <moukoublen@gmail.com>
2018-10-23 16:45:21 +0300

    add docker-machine in marmalade

--- envs/docker_env.sh
+++ envs/docker_env.sh
@@ -60,3 +60,22 @@ _marmalade_install__compose() {
   curl -L --fail https://github.com/docker/compose/releases/download/$1/docker-compose-Linux-x86_64 > $MARMALADE_ENVS/compose/$1/docker-compose
   chmod +x $MARMALADE_ENVS/compose/$1/docker-compose
 }
+
+
+
+
+_marmalade_get_latest_version_number__docker-machine() {
+  curl -s https://api.github.com/repos/docker/machine/releases/latest | jq -r '.name'
+}
+
+_marmalade_get_local_latest_version_number__docker-machine() {
+  ls -1 $MARMALADE_ENVS/docker-machine/ | grep -v default | sort -V | tail -n 1
+}
+
+_marmalade_install__docker-machine() {
+  # $1: version e.g. 1.16.1
+  # https://github.com/docker/compose/releases/download/1.16.1/docker-compose-Linux-x86_64
+  mkdir -p $MARMALADE_ENVS/docker-machine/$1
+  curl -L --fail https://github.com/docker/machine/releases/download/$1/docker-machine-$(uname -s)-$(uname -m) > $MARMALADE_ENVS/docker-machine/$1/docker-machine
+  chmod +x $MARMALADE_ENVS/docker-machine/$1/docker-machine
+}
