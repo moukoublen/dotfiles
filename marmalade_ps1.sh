e7314e967e94b621bd501ded6a7ef5a0da0bd613
Kostas Stamatakis <moukoublen@gmail.com>
2019-03-12 12:33:56 +0200

    update ps1 script

--- marmalade_ps1.sh
+++ marmalade_ps1.sh
@@ -14,21 +14,77 @@ export GIT_PS1_SHOWCOLORHINTS=true
 _marmalade_ps1() {
   [[ -z $BASH ]] && return
 
-  local F_SEP='\[\033[2;37m\]'
-  local F_PATH='\[\033[32m\]'
-  local F_GIT='\[\033[1;93m\]'
-  local F_MAIN='\[\033[36m\]'
-  local RESET_ALL='\[\033[0m\]'
+  ## [format];[background color];[foreground color]
 
-  local p_s1="${F_MAIN}╭${RESET_ALL}"
-  local p_s2="${F_MAIN}╰${RESET_ALL}"
-  local p_sep="${F_SEP}|${RESET_ALL}"
-  local p_git="\$(declare -F __git_ps1 &>/dev/null && __git_ps1 \"${p_sep} ${F_GIT}%s${RESET_ALL}\")"
-  local p_pwd="${F_PATH}\w${RESET_ALL}"
-  local p_usr="${F_MAIN}\u@\h${RESET_ALL}"
+  ##Format
+  ##    1 Bold/Bright
+  ##    2 Dim
+  ##    4 Underlined
+  ##    5 Blink
+  ##    7 Reverse
+  ##    8 Hidden
+  ##    0 Reset all attributes
+  ##   21 Reset bold/bright
+  ##   22 Reset dim
+  ##   24 Reset underlined
+  ##   25 Reset blink
+  ##   27 Reset reverse
+  ##   28 Reset hidden
 
-  local ps1_line1="${p_s1} ${p_usr} ${p_sep} ${p_pwd} ${p_git}"
-  local ps1_line2="${p_s2} "
+  ##Foreground
+  ##   39 Default foreground color
+  ##   30 Black
+  ##   31 Red
+  ##   32 Green
+  ##   33 Yellow
+  ##   34 Blue
+  ##   35 Magenta
+  ##   36 Cyan
+  ##   37 Light gray
+  ##   90 Dark gray
+  ##   91 Light red
+  ##   92 Light green
+  ##   93 Light yellow
+  ##   94 Light blue
+  ##   95 Light magenta
+  ##   96 Light cyan
+  ##   97 White
+
+  ##Background
+  ##   49 Default background color
+  ##   40 Black
+  ##   41 Red
+  ##   42 Green
+  ##   43 Yellow
+  ##   44 Blue
+  ##   45 Magenta
+  ##   46 Cyan
+  ##   47 Light gray
+  ##  100 Dark gray
+  ##  101 Light red
+  ##  102 Light green
+  ##  103 Light yellow
+  ##  104 Light blue
+  ##  105 Light magenta
+  ##  106 Light cyan
+  ##  107 White
+
+  local C_SEP='\[\033[2;49;37m\]'
+  local F_PTH='\[\033[0;49;32m\]'
+  local F_GIT='\[\033[0;49;93m\]'
+  local F_MAI='\[\033[0;49;36m\]'
+  local F_RST='\[\033[0;49;39m\]'
+
+  local p_st1="${F_MAI}╭${F_RST}"
+  local p_st2="${F_MAI}╰${F_RST}"
+  local p_sep="${C_SEP}|${F_RST}"
+  local p_gfr="${p_sep} ${F_GIT}%s${F_RST}" #__git_ps1 format string
+  local p_git="\$(declare -F __git_ps1 &>/dev/null && __git_ps1 \"${p_gfr}\")"
+  local p_pwd="${F_PTH}\w${F_RST}"
+  local p_usr="${F_MAI}\u@\h${F_RST}"
+
+  local ps1_line1="${p_st1} ${p_usr} ${p_sep} ${p_pwd} ${p_git}"
+  local ps1_line2="${p_st2} "
 
   export PS1="${ps1_line1}\n${ps1_line2}"
 }
