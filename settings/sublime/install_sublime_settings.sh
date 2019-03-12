7a9f07603435884e96f63aef3b32582d820d6079
Kostas Stamatakis <moukoublen@gmail.com>
2019-03-13 00:35:33 +0200

    sublime settings

---
+++ settings/sublime/install_sublime_settings.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.config/sublime-text-3/Packages/User
+cp ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings.backup
+rm ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+ln -s $MARMALADE_PATH/settings/sublime/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/
