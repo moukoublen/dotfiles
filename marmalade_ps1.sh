7da7306911a9a07074ba080b427ecec8179b8cbe
Kostas Stamatakis <moukoublen@gmail.com>
2019-10-26 16:06:32 +0300

    update vscode and ps1

--- marmalade_ps1.sh
+++ marmalade_ps1.sh
@@ -7,6 +7,15 @@ export GIT_PS1_SHOWUNTRACKEDFILES=true
 export GIT_PS1_SHOWUPSTREAM="verbose name"
 export GIT_PS1_SHOWCOLORHINTS=true
 
+
+__user_ps1_c() {
+  if [[ $UID = 0 ]]; then
+    echo -e "\e[1;49;91m"
+  else
+    echo -e "\e[0;49;36m"
+  fi
+}
+
 _marmalade_ps1() {
   [[ -z $BASH ]] && return
 
@@ -65,11 +74,13 @@ _marmalade_ps1() {
   ##  106 Light cyan
   ##  107 White
 
-  local C_SEP='\[\033[2;49;37m\]'
-  local F_PTH='\[\033[0;49;32m\]'
-  local F_GIT='\[\033[0;49;93m\]'
-  local F_MAI='\[\033[0;49;36m\]'
-  local F_RST='\[\033[0;49;39m\]'
+  local C_SEP="\e[2;49;37m"
+  local C_STA="\e[2;49;97m"
+  local F_PTH="\e[0;49;32m"
+  local F_GIT="\e[0;49;93m"
+  local F_MAI="\e[0;49;36m"
+  local F_ROO="\e[1;49;91m"
+  local F_RST="\e[0m"
 
   local p_st1="${F_MAI}╭${F_RST}"
   local p_st2="${F_MAI}╰${F_RST}"
@@ -77,7 +88,7 @@ _marmalade_ps1() {
   local p_gfr="${p_sep} ${F_GIT}%s${F_RST}" #__git_ps1 format string
   local p_git="\$(declare -F __git_ps1 &>/dev/null && __git_ps1 \"${p_gfr}\")"
   local p_pwd="${F_PTH}\w${F_RST}"
-  local p_usr="${F_MAI}\u@\h${F_RST}"
+  local p_usr="\$(if [[ \$UID = 0 ]]; then echo -e \"${F_ROO}\"; else echo -e \"${F_MAI}\"; fi)\u${F_RST}"
 
   local ps1_line1="${p_st1} ${p_usr} ${p_sep} ${p_pwd} ${p_git}"
   local ps1_line2="${p_st2} "
