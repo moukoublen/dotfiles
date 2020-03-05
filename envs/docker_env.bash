57495c1ecdb27a81ffb4c547c78e6223cfa46bc8
Kostas Stamatakis <moukoublen@gmail.com>
2020-03-05 10:54:14 +0200

    update env compose to docker-compose

--- envs/docker_env.bash
+++ envs/docker_env.bash
@@ -45,20 +45,20 @@ fi
 
 ################################################################################
 ################# marmalade functions ##########################################
-_marmalade_get_latest_version_number__compose() {
+_marmalade_get_latest_version_number__docker-compose() {
   curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.name'
 }
 
-_marmalade_get_local_latest_version_number__compose() {
-  ls -1 $MARMALADE_ENVS/compose/ | grep -v default | sort -V | tail -n 1
+_marmalade_get_local_latest_version_number__docker-compose() {
+  ls -1 $MARMALADE_ENVS/docker-compose/ | grep -v default | sort -V | tail -n 1
 }
 
-_marmalade_install__compose() {
+_marmalade_install__docker-compose() {
   # $1: version e.g. 1.16.1
   # https://github.com/docker/compose/releases/download/1.16.1/docker-compose-Linux-x86_64
-  mkdir -p $MARMALADE_ENVS/compose/$1
-  curl -L --fail https://github.com/docker/compose/releases/download/$1/docker-compose-Linux-x86_64 > $MARMALADE_ENVS/compose/$1/docker-compose
-  chmod +x $MARMALADE_ENVS/compose/$1/docker-compose
+  mkdir -p $MARMALADE_ENVS/docker-compose/$1
+  curl -L --fail https://github.com/docker/compose/releases/download/$1/docker-compose-Linux-x86_64 > $MARMALADE_ENVS/docker-compose/$1/docker-compose
+  chmod +x $MARMALADE_ENVS/docker-compose/$1/docker-compose
 }
 
 _marmalade_get_latest_version_number__docker-machine() {
