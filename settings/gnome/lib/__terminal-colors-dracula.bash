a00d36245b376fabf13ed9d8330bbfcfbbc9d739
Kostas Stamatakis <moukoublen@gmail.com>
2020-04-20 01:20:00 +0300

    update terminal colors

--- settings/gnome/lib/__terminal-colors-dracula.bash
+++ settings/gnome/lib/__terminal-colors-dracula.bash
@@ -4,4 +4,22 @@ export C_FG="#F8F8F2"
 export C_BG="#282936"
 export C_HFG="#FFFFFF"
 export C_HBG="#E91E63"
-export C_PAL="['#000000','#FF5454','#50FA7B','#F1FA8C','#BD93F9','#FF79C6','#8BE8FD','#BFBFBF','#4D4D4D','#FF6E67','#5AF78D','#F4F99D','#CAA8FA','#FF92D0','#9AEDFE','#E6E6E6']"
+
+export C_00='#000000'  # Black
+export C_01='#FF5454'  # Red
+export C_02='#50FA7B'  # Green
+export C_03='#F1FA8C'  # Yellow
+export C_04='#BD93F9'  # Blue
+export C_05='#FF79C6'  # Purple
+export C_06='#8BE8FD'  # Turquoise
+export C_07='#BFBFBF'  # Grey
+export C_08='#4D4D4D'  # Light Black
+export C_09='#FF6E67'  # Light Red
+export C_10='#5AF78D'  # Light Green
+export C_11='#F4F99D'  # Light Yellow
+export C_12='#CAA8FA'  # Light Blue
+export C_13='#FF92D0'  # Light Purple
+export C_14='#9AEDFE'  # Light Turquoise
+export C_15='#E6E6E6'  # Light Grey
+
+export C_PAL="['${C_00}','${C_01}','${C_02}','${C_03}','${C_04}','${C_05}','${C_06}','${C_07}','${C_08}','${C_09}','${C_10}','${C_11}','${C_12}','${C_13}','${C_14}','${C_15}']"
--- settings/gnome/lib/__terminal-colors.bash
+++ settings/gnome/lib/__terminal-colors.bash
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
-export C_FG="#E0E0E0"
-export C_BG="#212121"
-export C_HFG="#F5F5F5"
+export C_FG="#E0E0E0"  # (grey 300)
+export C_BG="#212121"  # (grey 900)
+export C_HFG="#F5F5F5" # (grey 100)
 export C_HBG="#C2185B"
-export C_PAL="['#424242', '#E53834', '#43A047', '#FDD835', '#1D87E5', '#8D24AA', '#00897A', '#90A4AE', '#757575', '#E57272', '#81C784', '#FFF176', '#64B5F6', '#BA68C8', '#4DB6AC', '#CFD8DC']"
+
+export C_00='#424242'  # Black (grey 800)
+export C_01='#E53834'  # Red
+export C_02='#43A047'  # Green (green 600)
+export C_03='#FDD835'  # Yellow (yellow 600)
+export C_04='#1D87E5'  # Blue
+export C_05='#8D24AA'  # Purple
+export C_06='#00897A'  # Turquoise
+export C_07='#90A4AE'  # Grey (blueGrey 300)
+export C_08='#757575'  # Light Black (grey 500)
+export C_09='#E57272'  # Light Red
+export C_10='#81C784'  # Light Green (green 300)
+export C_11='#FFF176'  # Light Yellow (yello 300)
+export C_12='#64B5F6'  # Light Blue (blue 300)
+export C_13='#BA68C8'  # Light Purple (purple 300)
+export C_14='#4DB6AC'  # Light Turquoise (teal 300)
+export C_15='#CFD8DC'  # Light Grey (blueGrey 100)
+
+export C_PAL="['${C_00}','${C_01}','${C_02}','${C_03}','${C_04}','${C_05}','${C_06}','${C_07}','${C_08}','${C_09}','${C_10}','${C_11}','${C_12}','${C_13}','${C_14}','${C_15}']"
