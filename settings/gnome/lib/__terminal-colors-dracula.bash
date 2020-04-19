0d9e94647da4df8b1a7653de7862c7c0e1179991
Kostas Stamatakis <moukoublen@gmail.com>
2020-04-20 00:41:58 +0300

    add terminal colors

---
+++ settings/gnome/lib/__terminal-colors-dracula.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export C_FG="#F8F8F2"
+export C_BG="#282936"
+export C_HFG="#FFFFFF"
+export C_HBG="#E91E63"
+export C_PAL="['#000000','#FF5454','#50FA7B','#F1FA8C','#BD93F9','#FF79C6','#8BE8FD','#BFBFBF','#4D4D4D','#FF6E67','#5AF78D','#F4F99D','#CAA8FA','#FF92D0','#9AEDFE','#E6E6E6']"
