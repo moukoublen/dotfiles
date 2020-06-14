bf843e26ad44b0b4876d77cd7725fb957a148127
Kostas Stamatakis <moukoublen@gmail.com>
2020-06-14 15:27:54 +0300

    updates

--- lib/common.bash
+++ lib/common.bash
@@ -1,6 +1,9 @@
-alias S='find . -name'
 alias to-ack='ack --files-from=-' #pipe find results
 alias to-grep='xargs grep --color=auto' #pipe find results
 alias path-to-lines='echo $PATH | tr ":" "\n"'
 alias ld-to-lines='echo $LD_LIBRARY_PATH | tr ":" "\n"'
 alias update-dotfiles="(cd ${MARMALADE_PATH}; git pull)"
+
+S() {
+  find . -name "*$@*"
+}
