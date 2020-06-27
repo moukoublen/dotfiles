0a4a0c0c8f2ef82710a9d3ff434a58865a7e6a99
Kostas Stamatakis <moukoublen@gmail.com>
2020-06-27 22:54:44 +0300

    update terminal colors

--- ext/settings/gnome/lib/__terminal-colors.bash
+++ ext/settings/gnome/lib/__terminal-colors.bash
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-export C_FG="#E0E0E0"  # (grey 300)
-export C_BG="#212121"  # (grey 900)
-export C_HFG="#F5F5F5" # (grey 100)
-export C_HBG="#C2185B"
+export C_FG="#f5f5f5"  # (grey 100)
+export C_BG="#24292E"  #
+export C_HFG="#FAFAFA" # (grey 50)
+export C_HBG="#ff4081" # (pink A 200)
 
 export C_00='#424242'  # Black (grey 800)
-export C_01='#E53834'  # Red
-export C_02='#43A047'  # Green (green 600)
-export C_03='#FDD835'  # Yellow (yellow 600)
-export C_04='#1D87E5'  # Blue
-export C_05='#8D24AA'  # Purple
-export C_06='#00897A'  # Turquoise
-export C_07='#90A4AE'  # Grey (blueGrey 300)
-export C_08='#757575'  # Light Black (grey 500)
-export C_09='#E57272'  # Light Red
-export C_10='#81C784'  # Light Green (green 300)
-export C_11='#FFF176'  # Light Yellow (yello 300)
-export C_12='#64B5F6'  # Light Blue (blue 300)
+export C_01='#F44336'  # Red (red 500)
+export C_02='#8BC34A'  # Green (lightGreen 500)
+export C_03='#FFEB3B'  # Yellow (yellow 500)
+export C_04='#03A9F4'  # Blue (lightBlue 500)
+export C_05='#9C27B0'  # Purple (purple 500)
+export C_06='#009688'  # Turquoise (teal 500)
+export C_07='#607D8B'  # Grey (blueGrey 500)
+export C_08='#9E9E9E'  # Light Black (grey 500)
+export C_09='#E57373'  # Light Red (red 300)
+export C_10='#AED581'  # Light Green (lightGreen 300)
+export C_11='#FFF176'  # Light Yellow (yellow 300)
+export C_12='#4FC3F7'  # Light Blue (lightBlue 300)
 export C_13='#BA68C8'  # Light Purple (purple 300)
 export C_14='#4DB6AC'  # Light Turquoise (teal 300)
-export C_15='#CFD8DC'  # Light Grey (blueGrey 100)
+export C_15='#90A4AE'  # Light Grey (blueGrey 300)
 
 export C_PAL="['${C_00}','${C_01}','${C_02}','${C_03}','${C_04}','${C_05}','${C_06}','${C_07}','${C_08}','${C_09}','${C_10}','${C_11}','${C_12}','${C_13}','${C_14}','${C_15}']"
