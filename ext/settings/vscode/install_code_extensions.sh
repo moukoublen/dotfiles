8a9ca348a12dac38f6b07b99c5229ce0581e250d
Kostas Stamatakis <moukoublen@gmail.com>
2020-07-04 18:18:00 +0300

    update vscode extensions

--- ext/settings/vscode/install_code_extensions.sh
+++ ext/settings/vscode/install_code_extensions.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 extensions=(
+asciidoctor.asciidoctor-vscode
 dracula-theme.theme-dracula
+eamodio.gitlens
 GitHub.github-vscode-theme
+golang.go
 ms-azuretools.vscode-docker
 ms-python.python
-ms-vscode.Go
+ms-vscode.cpptools
 PKief.material-icon-theme
 scala-lang.scala
 scala-lang.scala-snippets
-vscode-icons-team.vscode-icons
 vscoss.vscode-ansible
 )
 
