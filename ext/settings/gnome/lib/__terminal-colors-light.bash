754c0759253491d2535228a5eda876bcde7b3469
Kostas Stamatakis <moukoublen@gmail.com>
2020-08-09 00:54:59 +0300

    add terminal light colors; fix install fonts script

---
+++ ext/settings/gnome/lib/__terminal-colors-light.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+export C_FG="#212121"  # (grey 900)
+export C_BG="#e0e0e0"  # (grey 300)
+export C_HFG="#000000" # (full black)
+export C_HBG="#FF4081" # (pink A 200)
+
+export C_00='#424242'  # Black (grey 800)
+export C_01='#C62828'  # Red (red 800)
+export C_02='#558B2F'  # Green (lightGreen 800)
+export C_03='#fb8c00'  # Yellow (orange 800)
+export C_04='#0277BD'  # Blue (lightBlue 800)
+export C_05='#6A1B9A'  # Purple (purple 800)
+export C_06='#00695C'  # Turquoise (teal 800)
+export C_07='#37474F'  # Grey (blueGrey 800)
+export C_08='#757575'  # Light Black (grey 600)
+export C_09='#E53935'  # Light Red (red 600)
+export C_10='#689F38'  # Light Green (lightGreen 600)
+export C_11='#ef6c00'  # Light Yellow (orange 600)
+export C_12='#039BE5'  # Light Blue (lightBlue 600)
+export C_13='#8E24AA'  # Light Purple (purple 600)
+export C_14='#00897B'  # Light Turquoise (teal 600)
+export C_15='#546E7A'  # Light Grey (blueGrey 600)
+
+export C_PAL="['${C_00}','${C_01}','${C_02}','${C_03}','${C_04}','${C_05}','${C_06}','${C_07}','${C_08}','${C_09}','${C_10}','${C_11}','${C_12}','${C_13}','${C_14}','${C_15}']"
