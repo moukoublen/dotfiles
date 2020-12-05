d3821f76090d8b2877ec1096ab5de519dcf19c0c
Kostas Stamatakis <moukoublen@gmail.com>
2020-12-05 19:48:57 +0200

    update gnome and tilix settings

--- ext/settings/gnome/lib/gnome-shell-settings.bash
+++ ext/settings/gnome/lib/gnome-shell-settings.bash
@@ -17,10 +17,10 @@ gsettings-set "org.gnome.desktop.wm.keybindings switch-windows" "['<Alt>Tab']"
 gsettings-set "org.gnome.desktop.wm.preferences action-middle-click-titlebar"  'toggle-maximize-vertically'
 gsettings-set "org.gnome.desktop.wm.preferences button-layout" "appmenu:minimize,maximize,close"
 
-gsettings-set "org.gnome.desktop.interface font-name" "IBM Plex Sans 11"
-gsettings-set "org.gnome.desktop.interface document-font-name" "IBM Plex Sans 11"
-gsettings-set "org.gnome.desktop.interface monospace-font-name" "Hack 12"
-gsettings-set "org.gnome.desktop.wm.preferences titlebar-font" "IBM Plex Sans Medium 11"
+gsettings-set "org.gnome.desktop.interface font-name" "Iosevka Aile 11"
+gsettings-set "org.gnome.desktop.interface document-font-name" "Iosevka Aile 11"
+gsettings-set "org.gnome.desktop.interface monospace-font-name" "Iosevka 12"
+gsettings-set "org.gnome.desktop.wm.preferences titlebar-font" "Iosevka Etoile 11"
 
 gsettings-set "org.gnome.desktop.interface cursor-size" "32"
 echo "-----------------------------------"
