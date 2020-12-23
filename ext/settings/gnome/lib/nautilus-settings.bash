55cde8b0f1530c7e1e670babe0e0da1d7207311c
Kostas Stamatakis <moukoublen@gmail.com>
2020-12-24 01:19:46 +0200

    update nautilus settings

--- ext/settings/gnome/lib/nautilus-settings.bash
+++ ext/settings/gnome/lib/nautilus-settings.bash
@@ -1,12 +1,46 @@
 #!/usr/bin/env bash
 
-################### Nautilus ###################################################
-echo ""
-echo "----- Nautilus Settings -----"
-gsettings-set 'org.gnome.nautilus.list-view default-zoom-level' 'small'
-gsettings-set 'org.gnome.nautilus.list-view use-tree-view' true
-gsettings-set 'org.gnome.nautilus.preferences default-folder-viewer' 'list-view'
-gsettings-set 'org.gnome.nautilus.preferences show-delete-permanently' true
-gsettings-set 'org.gnome.nautilus.preferences show-hidden-files' true
-echo "-----------------------------------"
-################################################################################
+__help() {
+  echo "Usage: <set|get>"
+}
+
+func=
+case $1 in
+  get)
+    func="__get-settings"
+    ;;
+  set)
+    func="__set-settings"
+    ;;
+  *)
+    __help
+    exit 1
+    ;;
+esac
+
+declare -A settings
+settings=( \
+  ["org.gnome.nautilus.list-view default-zoom-level"]="small" \
+  ["org.gnome.nautilus.list-view use-tree-view"]="true" \
+  ["org.gnome.nautilus.preferences default-folder-viewer"]="list-view" \
+  ["org.gnome.nautilus.preferences show-delete-permanently"]="true" \
+  ["org.gnome.nautilus.preferences show-hidden-files"]="true" \
+)
+
+__set-settings() {
+  for key in "${!settings[@]}"; do
+    gsettings-set "${key}" "${settings[$key]}"
+  done
+}
+
+__gsettings-get() {
+  printf "\e[2m%s\e[0m \e[94m=>\e[0m \e[1;32m%s\e[0m\n" "$1" "$(gsettings get $1)"
+}
+
+__get-settings() {
+  for key in "${!settings[@]}"; do
+    __gsettings-get "${key}"
+  done
+}
+
+${func}
