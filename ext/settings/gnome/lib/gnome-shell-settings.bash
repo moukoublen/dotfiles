b36d36b707ee8158787cfd9f091405846f741c82
Kostas Stamatakis <moukoublen@gmail.com>
2020-12-24 01:22:50 +0200

    update gnome shell settings script

--- ext/settings/gnome/lib/gnome-shell-settings.bash
+++ ext/settings/gnome/lib/gnome-shell-settings.bash
@@ -1,27 +1,58 @@
 #!/usr/bin/env bash
 
-################### Gnome Shell and WM #########################################
-echo ""
-echo "----- Gnome Settings --------"
-gsettings-set "org.gnome.shell.keybindings focus-active-notification" "[]"
-gsettings-set "org.gnome.shell.keybindings toggle-message-tray" "[]"
+__help() {
+  echo "Usage: <set|get>"
+}
 
-gsettings-set "org.gnome.mutter.keybindings toggle-tiled-left" "['<Super>Left']"
-gsettings-set "org.gnome.mutter.keybindings toggle-tiled-right" "['<Super>Right']"
-gsettings-set "org.gnome.desktop.wm.keybindings maximize" "['<Super>Up']"
-gsettings-set "org.gnome.desktop.wm.keybindings unmaximize" "['<Super>Down']"
-gsettings-set "org.gnome.desktop.wm.keybindings maximize-vertically" "['<Shift><Alt>r']"
-gsettings-set "org.gnome.desktop.wm.keybindings maximize-horizontally" "['<Shift><Alt>e']"
-gsettings-set "org.gnome.desktop.wm.keybindings switch-applications" "['<Super>Tab']"
-gsettings-set "org.gnome.desktop.wm.keybindings switch-windows" "['<Alt>Tab']"
-gsettings-set "org.gnome.desktop.wm.preferences action-middle-click-titlebar"  'toggle-maximize-vertically'
-gsettings-set "org.gnome.desktop.wm.preferences button-layout" "appmenu:minimize,maximize,close"
+func=
+case $1 in
+  get)
+    func="__get-settings"
+    ;;
+  set)
+    func="__set-settings"
+    ;;
+  *)
+    __help
+    exit 1
+    ;;
+esac
 
-gsettings-set "org.gnome.desktop.interface font-name" "Iosevka Aile 11"
-gsettings-set "org.gnome.desktop.interface document-font-name" "Iosevka Aile 11"
-gsettings-set "org.gnome.desktop.interface monospace-font-name" "Iosevka 12"
-gsettings-set "org.gnome.desktop.wm.preferences titlebar-font" "Iosevka Etoile 11"
+declare -A settings
+settings=( \
+  ["org.gnome.shell.keybindings focus-active-notification"]="[]" \
+  ["org.gnome.shell.keybindings toggle-message-tray"]="[]" \
+  ["org.gnome.mutter.keybindings toggle-tiled-left"]="['<Super>Left']" \
+  ["org.gnome.mutter.keybindings toggle-tiled-right"]="['<Super>Right']" \
+  ["org.gnome.desktop.wm.keybindings maximize"]="['<Super>Up']" \
+  ["org.gnome.desktop.wm.keybindings unmaximize"]="['<Super>Down']" \
+  ["org.gnome.desktop.wm.keybindings maximize-vertically"]="['<Shift><Alt>r']" \
+  ["org.gnome.desktop.wm.keybindings maximize-horizontally"]="['<Shift><Alt>e']" \
+  ["org.gnome.desktop.wm.keybindings switch-applications"]="['<Super>Tab']" \
+  ["org.gnome.desktop.wm.keybindings switch-windows"]="['<Alt>Tab']" \
+  ["org.gnome.desktop.wm.preferences action-middle-click-titlebar"]="toggle-maximize-vertically" \
+  ["org.gnome.desktop.wm.preferences button-layout"]="appmenu:minimize,maximize,close" \
+  ["org.gnome.desktop.interface font-name"]="Iosevka Aile 11" \
+  ["org.gnome.desktop.interface document-font-name"]="Iosevka Aile 11" \
+  ["org.gnome.desktop.interface monospace-font-name"]="Iosevka 12" \
+  ["org.gnome.desktop.wm.preferences titlebar-font"]="Iosevka Etoile 11" \
+  ["org.gnome.desktop.interface cursor-size"]="32" \
+)
 
-gsettings-set "org.gnome.desktop.interface cursor-size" "32"
-echo "-----------------------------------"
-################################################################################
+__set-settings() {
+  for key in "${!settings[@]}"; do
+    gsettings-set "${key}" "${settings[$key]}"
+  done
+}
+
+__gsettings-get() {
+  printf "\e[2m%s\e[0m \e[94m=>\e[0m \e[1;32m%s\e[0m\n" "$1" "$(gsettings get $1)"
+}
+
+__get-settings() {
+  for key in "${!settings[@]}"; do
+    __gsettings-get "${key}"
+  done
+}
+
+${func}
