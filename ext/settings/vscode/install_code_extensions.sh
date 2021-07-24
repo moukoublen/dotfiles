ca0618776a04a7c9bd9373366b63264d0f4da2ea
Kostas Stamatakis <moukoublen@gmail.com>
2021-07-24 15:12:51 +0300

    update vscode settings

--- ext/settings/vscode/install_code_extensions.sh
+++ ext/settings/vscode/install_code_extensions.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
+
+# asciidoctor.asciidoctor-vscode
+# bierner.markdown-mermaid
+# dracula-theme.theme-dracula
+# rangav.vscode-thunder-client
+# scala-lang.scala
+# scala-lang.scala-snippets
+# streetsidesoftware.code-spell-checker
+
 extensions=(
-asciidoctor.asciidoctor-vscode
+arcticicestudio.nord-visual-studio-code
+attilathedud.data-converter
 eamodio.gitlens
 GitHub.github-vscode-theme
 golang.go
 ms-azuretools.vscode-docker
 ms-python.python
+ms-python.vscode-pylance
+ms-toolsai.jupyter
 ms-vscode.cpptools
 PKief.material-icon-theme
-scala-lang.scala
-scala-lang.scala-snippets
-vscoss.vscode-ansible
+recode4dev.jenkinsfile
+redhat.vscode-commons
+redhat.vscode-xml
 )
 
 for ext in "${extensions[@]}"
