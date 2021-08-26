703b6aa2c49f3ef4611315d8e6931a56489a2eb9
Kostas Stamatakis <moukoublen@gmail.com>
2021-08-26 23:55:50 +0300

    sublime

--- ext/settings/sublime/install_sublime_settings.sh
+++ ext/settings/sublime/install_sublime_settings.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.config/sublime-text-3/Packages/User
-cp ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings.backup
-rm ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
-ln -s $MARMALADE_PATH/ext/settings/sublime/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/
+mkdir -p ~/.config/sublime-text/Packages/User
+cp ~/.config/sublime-text/Packages/User/Preferences.sublime-settings ~/.config/sublime-text/Packages/User/Preferences.sublime-settings.backup
+rm ~/.config/sublime-text/Packages/User/Preferences.sublime-settings
+ln -s $MARMALADE_PATH/ext/settings/sublime/Preferences.sublime-settings ~/.config/sublime-text/Packages/User/
