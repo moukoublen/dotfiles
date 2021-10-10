4cef956099a4151c1f3a4943c589dd2ccc1c93a8
Kostas Stamatakis <moukoublen@gmail.com>
2021-10-10 16:38:23 +0300

    clean unused envs

--- lib/envs/core.bash
+++
@@ -1,3 +0,0 @@
-#export DOTNET_ROOT=$MARMALADE_ENVS/core/default
-#add_to_path $DOTNET_ROOT "after"
-
--- lib/envs/rbenv.bash
+++
@@ -1,9 +0,0 @@
-if command -v rbenv 1>/dev/null 2>&1; then
-  add_to_path $HOME/.rbenv/bin
-  eval "$(rbenv init -)"
-fi
-#alias update_rbenv='(cd ~/.rbenv/; git pull; cd ~/.rbenv/plugins/ruby-build/; git pull)'
-#install-rbenv() {
-#  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-#  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-#}
