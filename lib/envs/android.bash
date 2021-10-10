46a35a76a997c749c1a5f3540e87f1321ab695f4
Kostas Stamatakis <moukoublen@gmail.com>
2021-10-10 16:36:25 +0300

    updates to scripts and envs

--- lib/envs/android.bash
+++
@@ -1,4 +0,0 @@
-#export ANDR_PT="$HOME/Android/Sdk/platform-tools"
-#android-to-path() {
-#  add_to_path $ANDR_PT
-#}
--- lib/envs/aws.bash
+++ lib/envs/aws.bash
@@ -1,3 +1,14 @@
 if command -v aws_completer 1>/dev/null 2>&1; then
   complete -C "$(which aws_completer)" aws
 fi
+
+install-awscli2() {(
+  # To upgrade run with --update
+  tmp_dir=$(mktemp --directory)
+  cd $tmp_dir
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install "$@"
+  cd ~
+  rm -rf $tmp_dir
+)}
