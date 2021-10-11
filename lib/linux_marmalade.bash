ac38ab8a0fde466ff4cf87a756b35a6cc635afd8
Kostas Stamatakis <moukoublen@gmail.com>
2021-10-11 21:46:39 +0300

    update wcat

--- lib/linux_marmalade.bash
+++ lib/linux_marmalade.bash
@@ -18,7 +18,7 @@ alias PGREP='p | grep'
 ################################################################################
 ################################################################################
 
-wcat() {
+__wcat() {
   if [[ "$(type -t ${1})" == 'function' ]]; then
     declare -f "${1}"
     return
@@ -30,10 +30,32 @@ wcat() {
   local fl=$(which $1)
 
   echo -e "\e[0;90m============================================================\e[0m"
-  cat "${fl}"
-  echo -e "\e[0;90m============================================================\e[0m"
   echo -e "\e[0;37mfile:\e[0m \e[0;36m${fl}\e[0m"
   echo -e "\e[0;90m============================================================\e[0m"
+  cat "${fl}"
+  echo -e "\e[0;90m============================================================\e[0m"
+}
+
+wcat() {
+  local cat_cmd
+  cat_cmd=$(which bat)
+  if [[ "${cat_cmd}" == "" ]]; then
+    __wcat "${1}"
+    return
+  fi
+
+  cat_cmd="${cat_cmd} --paging=never --style=plain,header"
+
+  if [[ "$(type -t ${1})" == 'function' ]]; then
+    declare -f "${1}" | ${cat_cmd} --language=bash --file-name="${1}"
+    return
+  fi
+  if [[ "$(type -t ${1})" == 'alias' ]]; then
+    alias "${1}" | ${cat_cmd} --language=bash --file-name="${1}"
+    return
+  fi
+
+  ${cat_cmd} "$(which $1)"
 }
 complete -c wcat
 
