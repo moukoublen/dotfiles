f7e752ec4d7a31738ad6b6725a06c7b6d096a428
Kostas Stamatakis <moukoublen@gmail.com>
2021-11-01 21:54:08 +0200

    cleanup vscode extensions

--- ext/settings/vscode/install_code_extensions.sh
+++ ext/settings/vscode/install_code_extensions.sh
@@ -8,22 +8,24 @@
 # scala-lang.scala
 # scala-lang.scala-snippets
 # streetsidesoftware.code-spell-checker
+# ms-python.python
+# ms-python.vscode-pylance
+# ms-toolsai.jupyter
+# ms-vscode.cpptools
+# PKief.material-icon-theme
+# recode4dev.jenkinsfile
+# redhat.vscode-commons
+# redhat.vscode-xml
+# arcticicestudio.nord-visual-studio-code
+
+
 
 extensions=(
-arcticicestudio.nord-visual-studio-code
 attilathedud.data-converter
 eamodio.gitlens
 GitHub.github-vscode-theme
 golang.go
 ms-azuretools.vscode-docker
-ms-python.python
-ms-python.vscode-pylance
-ms-toolsai.jupyter
-ms-vscode.cpptools
-PKief.material-icon-theme
-recode4dev.jenkinsfile
-redhat.vscode-commons
-redhat.vscode-xml
 )
 
 for ext in "${extensions[@]}"
