c7df6b72b8be9a03226b08106f12f7036ab19fb7
Kostas Stamatakis <moukoublen@gmail.com>
2021-11-24 22:11:06 +0200

    updates

--- config/gnome/lib/__terminal-colors-primer_light.bash
+++ config/gnome/lib/__terminal-colors-primer_light.bash
@@ -3,30 +3,30 @@
 ## Primer Light Mode
 source __colors-primer.bash
 
-export C_FG=${primer_light['black']}
-export C_BG=${primer_light['gray-2']}
+export C_FG=${primer_light['gray-8']}
+export C_BG=${primer_light['gray-0']}
 export C_HFG=${primer_light['gray-0']}
 export C_HBG=${primer_light['pink-5']}
 export C_CFG=${primer_light['gray-0']}
-export C_CBG=${primer_light['pink-5']}
+export C_CBG=${primer_light['pink-6']}
 
-export C_00=${primer_light['gray-8']}   # Black
+export C_00=${primer_light['black']}   # Black
 export C_08=${primer_light['gray-7']}   # Light Black
 export C_07=${primer_light['gray-5']}   # Grey
 export C_15=${primer_light['gray-4']}   # Light Grey
 
-export C_01=${primer_light['red-8']}    # Red
-export C_09=${primer_light['red-6']}    # Light Red
-export C_02=${primer_light['green-8']}  # Green
-export C_10=${primer_light['green-6']}  # Light Green
-export C_03=${primer_light['yellow-8']} # Yellow
-export C_11=${primer_light['yellow-6']} # Light Yellow
-export C_04=${primer_light['blue-8']}   # Blue
-export C_12=${primer_light['blue-6']}   # Light Blue
-export C_05=${primer_light['purple-8']} # Purple
-export C_13=${primer_light['purple-6']} # Light Purple
-export C_06=${primer_light['pink-8']}   # Turquoise
-export C_14=${primer_light['pink-6']}   # Light Turquoise
+export C_01=${primer_light['red-4']}    # Red
+export C_09=${primer_light['red-3']}    # Light Red
+export C_02=${primer_light['green-4']}  # Green
+export C_10=${primer_light['green-3']}  # Light Green
+export C_03=${primer_light['yellow-4']} # Yellow
+export C_11=${primer_light['yellow-3']} # Light Yellow
+export C_04=${primer_light['blue-4']}   # Blue
+export C_12=${primer_light['blue-3']}   # Light Blue
+export C_05=${primer_light['purple-4']} # Purple
+export C_13=${primer_light['purple-3']} # Light Purple
+export C_06=${primer_light['pink-4']}   # Turquoise
+export C_14=${primer_light['pink-3']}   # Light Turquoise
 
 
 export C_PAL="['${C_00}','${C_01}','${C_02}','${C_03}','${C_04}','${C_05}','${C_06}','${C_07}','${C_08}','${C_09}','${C_10}','${C_11}','${C_12}','${C_13}','${C_14}','${C_15}']"
