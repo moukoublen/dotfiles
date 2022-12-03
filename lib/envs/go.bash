c75257b8d187b64ae4ce07bf119dc264567b279f
Kostas Stamatakis <moukoublen@gmail.com>
2022-12-03 12:50:31 +0200

    update go settings

--- lib/envs/go.bash
+++ lib/envs/go.bash
@@ -2,8 +2,8 @@ if [ -d /usr/local/go ]; then
   # No need of GOROOT in case of default path (/usr/local/go)
   #export GOROOT=/path/to/special/go
   add_to_path /usr/local/go/bin
-  export GOPATH=$HOME/goworkspace
-  add_to_path $GOPATH/bin
+  export GOPATH="${HOME}/goworkspace"
+  add_to_path "$(go env GOPATH)/bin"
 fi
 
 go-install() {
