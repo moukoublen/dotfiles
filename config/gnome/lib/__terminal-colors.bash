4bdc0e7a6e55177914ab30e1346e4ab37b478b10
Kostas Stamatakis <moukoublen@gmail.com>
2023-01-09 13:22:30 +0200

    mui colors for terminal

--- config/gnome/lib/__terminal-colors.bash
+++ config/gnome/lib/__terminal-colors.bash
@@ -3,30 +3,29 @@
 ## Material colors
 source __colors-material.bash
 
-export C_FG="${material['BlueGray-50']}"
-export C_BG="${material['Gray-900']}"
-export C_HFG="${material['White']}"
+export C_FG="${material['BlueGrey-50']}"
+export C_BG="${material['BlueGrey-900']}"
+export C_HFG="${material['Common-white']}"
 export C_HBG="${material['Pink-A700']}"
 export C_CFG="${material['Purple-600']}"
 export C_CBG="${material['Pink-400']}"
 
-export C_00="${material['Gray-800']}"  # Black
-export C_08="${material['Gray-500']}"  # Light Black
-export C_07="${material['BlueGray-500']}"  # Grey
-export C_15="${material['BlueGray-300']}"  # Light Grey
-
-export C_01="${material['Red-400']}"  # Red
-export C_09="${material['Red-300']}"  # Light Red
-export C_02="${material['Green-400']}"  # Green
-export C_10="${material['Green-300']}"  # Light Green
-export C_03="${material['Yellow-700']}"  # Yellow
-export C_11="${material['Yellow-600']}"  # Light Yellow
-export C_04="${material['Blue-400']}"  # Blue
-export C_12="${material['Blue-300']}"  # Light Blue
+export C_00="${material['Grey-800']}"        # Black
+export C_08="${material['Grey-500']}"        # Light Black
+export C_07="${material['BlueGrey-500']}"    # Grey
+export C_15="${material['BlueGrey-300']}"    # Light Grey
+export C_01="${material['Red-400']}"         # Red
+export C_09="${material['Red-300']}"         # Light Red
+export C_02="${material['Green-400']}"       # Green
+export C_10="${material['Green-300']}"       # Light Green
+export C_03="${material['Yellow-700']}"      # Yellow
+export C_11="${material['Yellow-600']}"      # Light Yellow
+export C_04="${material['Blue-400']}"        # Blue
+export C_12="${material['Blue-300']}"        # Light Blue
 export C_05="${material['DeepPurple-400']}"  # Purple
 export C_13="${material['DeepPurple-300']}"  # Light Purple
-export C_06="${material['Pink-400']}"  # Turquoise
-export C_14="${material['Pink-300']}"  # Light Turquoise
+export C_06="${material['Pink-400']}"        # Turquoise
+export C_14="${material['Pink-300']}"        # Light Turquoise
 
 
 export C_PAL="['${C_00}','${C_01}','${C_02}','${C_03}','${C_04}','${C_05}','${C_06}','${C_07}','${C_08}','${C_09}','${C_10}','${C_11}','${C_12}','${C_13}','${C_14}','${C_15}']"
--- config/gnome/lib/palettes/material.js
+++ config/gnome/lib/palettes/material.js
@@ -1,7 +1,51 @@
-import { HCT } from "@material/material-color-utilities";
+import colors from '@mui/material/node/colors/index.js';
+import { formatMuiErrorMessage } from '@mui/utils';
 
-// Simple demonstration of HCT.
-const color = HCT.fromInt(0xff4285f4);
-console.log(`Hue: ${color.hue}`);
-console.log(`Chrome: ${color.chroma}`);
-console.log(`Tone: ${color.tone}`);
+const capitalizeFirstLetter = x => x.charAt(0).toUpperCase() + x.slice(1);
+const compare = (r, l) => {
+    if (r.color === l.color) {
+        const rightAccent = Number(r.accent)
+        const leftAccent = Number(l.accent)
+        if (rightAccent < leftAccent) {
+            return -1;
+        }
+        if (rightAccent > leftAccent) {
+            return 1;
+        }
+        if (r.accent < l.accent) {
+            return -1;
+        }
+        if (r.accent > l.accent) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    return r.color.localeCompare(l.color);
+};
+
+console.log('#!/usr/bin/env bash');
+console.log('');
+console.log('# To generate this file run:');
+console.log('#    npm run material --silent --prefix ./palettes > __colors-material.bash');
+console.log('');
+console.log('declare -A material');
+console.log('');
+
+Object.entries(colors).flatMap(([colorName, values]) =>
+    Object.entries(values).map(([accent, value]) => {
+        return {
+            color: colorName,
+            accent: accent,
+            key: `${capitalizeFirstLetter(colorName)}-${accent}`,
+            value: value.toUpperCase(),
+        }
+    })
+)
+.sort(compare)
+.map((x) => `material['${x.key}']='${x.value}'`)
+.forEach(x => console.log(x));
+
+
+console.log('');
