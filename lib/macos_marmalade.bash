6f53babe7cd924b8f78de66a616192275ee7b08c
Kostas Stamatakis <moukoublen@gmail.com>
2023-04-12 14:22:36 +0300

    mac updates

--- lib/macos_marmalade.bash
+++ lib/macos_marmalade.bash
@@ -1,52 +1,44 @@
 ################################################################################
 ##################### Homebrew #################################################
-## Settings #####################################
-M_BREW_COREUTILS_USED=false
-M_BREW_FINDUTILS_USED=false
-#################################################
 M_HAS_BREW_INSTALLED=false
 M_BREW_PREFIX=""
 if command -v brew 1>/dev/null 2>&1; then
   M_HAS_BREW_INSTALLED=true
   M_BREW_PREFIX=$(brew --prefix) # /usr/local
+  add_to_path /usr/local/sbin
 fi
 
-if [[ -r "${M_BREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
+if [[ $M_HAS_BREW_INSTALLED = true ]] && [[ -r "${M_BREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
   export BASH_COMPLETION_COMPAT_DIR="${M_BREW_PREFIX}/etc/bash_completion.d"
   source "${M_BREW_PREFIX}/etc/profile.d/bash_completion.sh"
 fi
 
 # brew install coreutils (e.g.: cat, chmod, chroot, cp, dd, dir, du, echo, ls)
-if [[ $M_BREW_COREUTILS_USED = true ]]; then
-  add_to_path /usr/local/opt/coreutils/libexec/gnubin
-fi
+#add_to_path /usr/local/opt/coreutils/libexec/gnubin
 
 # brew install findutils (find, locate, updatedb, xargs)
-if [[ $M_BREW_FINDUTILS_USED = true ]]; then
-  add_to_path /usr/local/opt/findutils/libexec/gnubin
-fi
+#add_to_path /usr/local/opt/findutils/libexec/gnubin
 
-if [[ $M_HAS_BREW_INSTALLED = true ]] && [[ -f "${M_BREW_PREFIX}/opt/nvm/nvm.sh" ]]; then
-  export NVM_DIR=~/.nvm
-  source "${M_BREW_PREFIX}/opt/nvm/nvm.sh"
-  [[ -s "${M_BREW_PREFIX}/opt/nvm/etc/bash_completion.d/nvm" ]] && source "${M_BREW_PREFIX}/opt/nvm/etc/bash_completion.d/nvm"
-fi
+# brew install grep
+#add_to_path /usr/local/opt/grep/libexec/gnubin
 ################################################################################
 ################################################################################
-
 
 #export JAVA_HOME='/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home'
 
-#alias atom='/Applications/Atom.app/Contents/Resources/app/atom.sh'
 alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
 #alias code='/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
 alias cleanup_finder_shit="find . -type f -name '*.DS_Store' -ls -delete"
 alias ldd='otool -L'
-alias grep='command grep --color=auto'
 alias tree='command tree -C'
-if [[ $M_BREW_COREUTILS_USED = true ]]; then
-  alias ll='command ls -la --color=auto'
-  alias ls='command ls --color=auto'
+if [[ -d /usr/local/opt/grep/libexec/gnubin ]]; then
+  alias grep='command ggrep --color=auto'
+else
+  alias grep='command grep --color=auto'
+fi
+if [[ -d /usr/local/opt/coreutils/libexec/gnubin ]]; then
+  alias ll='command gls -la --color=auto'
+  alias ls='command gls --color=auto'
 else
   alias ll='command ls -laG'
   alias ls='command ls -G'
@@ -57,13 +49,11 @@ export LC_ALL='en_US.UTF-8';
 
 add_to_path $HOME/bin
 
-#source $DOTFILES_PATH/lib/envs/pyenv_env.sh
-#source $DOTFILES_PATH/lib/envs/rbenv_env.sh
 source $DOTFILES_PATH/lib/envs/go.bash
 source $DOTFILES_PATH/lib/envs/docker.bash
-#source $DOTFILES_PATH/lib/envs/php.sh
+source $DOTFILES_PATH/lib/envs/kube.bash
 
-install_nano_nanorc() {
+install-nano-nanorc() {
   touch ~/.nanorc
   for rcfile in $(brew --prefix nano)/share/nano/*.nanorc; do
     echo "include $rcfile" >> ~/.nanorc
