7ce81facffbfbd571ea63a50d4dfa5737a15d5e9
Kostas Stamatakis <moukoublen@gmail.com>
2023-05-19 17:17:52 +0300

    remove c alias

--- marmalade.bash
+++ marmalade.bash
@@ -36,7 +36,6 @@ alias to-ack='ack --files-from=-' #pipe find results
 alias to-grep='xargs grep --color=auto' #pipe find results
 alias path-to-lines='echo $PATH | tr ":" "\n"'
 alias ld-to-lines='echo $LD_LIBRARY_PATH | tr ":" "\n"'
-alias c='clear'
 S() {
   find . -name "*$@*"
 }
