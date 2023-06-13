906177b7511d3d264a2e6552e318bf8b21f59117
Kostas Stamatakis <moukoublen@gmail.com>
2023-06-14 00:44:02 +0300

    updatre go tools

--- lib/envs/go.bash
+++ lib/envs/go.bash
@@ -15,7 +15,8 @@ go-install() {
   echo -e "> Installing \e[0;90m$@\e[0m"
   (
     set -x
-    go install -a -trimpath -ldflags '-s -w' "$@"
+    #go install -a -trimpath -ldflags '-s -w' "$@"
+    go install -trimpath -ldflags '-s -w' "$@"
   )
 }
 
@@ -31,7 +32,7 @@ install-go-tools() {(
   #}
 
   # https://pkg.go.dev/golang.org/x/tools?tab=versions
-  GO_X_TOOLS_VER=v0.9.3
+  GO_X_TOOLS_VER=latest
   go-install "golang.org/x/tools/cmd/callgraph@${GO_X_TOOLS_VER}"
   go-install "golang.org/x/tools/cmd/digraph@${GO_X_TOOLS_VER}"
   go-install "golang.org/x/tools/cmd/fiximports@${GO_X_TOOLS_VER}"
@@ -47,7 +48,7 @@ install-go-tools() {(
   #__install_binary "${GOPATH}"/bin/{goimports,fieldalignment,shadow}
 
   # https://github.com/dominikh/go-tools/releases (https://staticcheck.io/)
-  HONNEF_VER=2023.1.3
+  HONNEF_VER=latest
   go-install "honnef.co/go/tools/cmd/staticcheck@${HONNEF_VER}"
   go-install "honnef.co/go/tools/cmd/structlayout@${HONNEF_VER}"
   go-install "honnef.co/go/tools/cmd/structlayout-pretty@${HONNEF_VER}"
@@ -55,40 +56,42 @@ install-go-tools() {(
   #__install_binary "${GOPATH}"/bin/{staticcheck,structlayout,structlayout-pretty,structlayout-optimize}
 
   # https://github.com/golangci/golangci-lint/releases
-  go-install 'github.com/golangci/golangci-lint/cmd/golangci-lint@v1.53.1'
+  go-install 'github.com/golangci/golangci-lint/cmd/golangci-lint@latest'
   #__install_binary "${GOPATH}"/bin/golangci-lint
 
   # https://github.com/securego/gosec/releases
-  go-install 'github.com/securego/gosec/v2/cmd/gosec@v2.16.0'
+  go-install 'github.com/securego/gosec/v2/cmd/gosec@latest'
   #__install_binary "${GOPATH}"/bin/gosec
 
   # https://github.com/mvdan/gofumpt/releases
-  go-install 'mvdan.cc/gofumpt@v0.5.0'
+  go-install 'mvdan.cc/gofumpt@latest'
   #__install_binary "${GOPATH}"/bin/gofumpt
 
   # https://github.com/mgechev/revive/releases
-  go-install 'github.com/mgechev/revive@v1.3.2'
+  go-install 'github.com/mgechev/revive@latest'
   #__install_binary "${GOPATH}"/bin/revive
 
   ################################################################
   ################################################################
 
   # https://github.com/mozilla/sops/releases
-  go-install 'go.mozilla.org/sops/v3/cmd/sops@v3.7.3'
+  go-install 'go.mozilla.org/sops/v3/cmd/sops@latest'
   #__install_binary "${GOPATH}"/bin/sops
 
   # https://github.com/orijtech/structslop/tags
-  go-install 'github.com/orijtech/structslop/cmd/structslop@v0.0.8'
+  go-install 'github.com/orijtech/structslop/cmd/structslop@latest'
   #__install_binary "${GOPATH}"/bin/structslop
 
   # https://github.com/jesseduffield/lazydocker/releases
-  #go-install github.com/jesseduffield/lazydocker@v0.20.0
+  #go-install github.com/jesseduffield/lazydocker@latest
 
   # https://github.com/jesseduffield/lazygit/releases
-  #go-install github.com/jesseduffield/lazygit@v0.37.0
+  #go-install github.com/jesseduffield/lazygit@latest
 
   # # https://github.com/gohugoio/hugo/releases
   # go-install -tags extended github.com/gohugoio/hugo@latest
+
+  go-install github.com/google/gops@latest
 )}
 
 # Where is the go ENV file?
