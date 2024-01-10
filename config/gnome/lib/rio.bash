c9f3e87c246c3873904bab9ccda96b5e5624e620
Kostas Stamatakis <moukoublen@gmail.com>
2024-01-10 23:33:01 +0200

    updates

---
+++ config/gnome/lib/rio.bash
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+source __colors-primer.bash
+
+echo "
+tabs='${primer_dark_dimmed['pink-8']}'
+tabs-active='${primer_dark_dimmed['pink-6']}'
+selection-foreground='${primer_dark_dimmed['gray-0']}'
+selection-background='${primer_dark_dimmed['pink-7']}'
+
+foreground='${primer_dark_dimmed['white']}'
+background='${primer_dark_dimmed['black']}'
+cursor='${primer_dark_dimmed['purple-8']}'
+
+black='${primer_dark_dimmed['gray-6']}'
+light-black='${primer_dark_dimmed['gray-4']}'
+dim-black='${primer_dark_dimmed['gray-6']}'
+
+white='${primer_dark_dimmed['gray-2']}'
+light-white='${primer_dark_dimmed['gray-0']}'
+dim-white='${primer_dark_dimmed['gray-2']}'
+
+red='${primer_dark_dimmed['red-4']}'
+light-red='${primer_dark_dimmed['red-3']}'
+dim-red='${primer_dark_dimmed['red-4']}'
+
+green='${primer_dark_dimmed['green-4']}'
+light-green='${primer_dark_dimmed['green-3']}'
+dim-green='${primer_dark_dimmed['green-4']}'
+
+yellow='${primer_dark_dimmed['yellow-4']}'
+light-yellow='${primer_dark_dimmed['yellow-3']}'
+dim-yellow='${primer_dark_dimmed['yellow-4']}'
+
+blue='${primer_dark_dimmed['blue-4']}'
+light-blue='${primer_dark_dimmed['blue-3']}'
+dim-blue='${primer_dark_dimmed['blue-4']}'
+
+magenta='${primer_dark_dimmed['purple-4']}'
+light-magenta='${primer_dark_dimmed['purple-3']}'
+dim-magenta='${primer_dark_dimmed['purple-4']}'
+
+cyan='${primer_dark_dimmed['pink-4']}'
+light-cyan='${primer_dark_dimmed['pink-3']}'
+dim-cyan='${primer_dark_dimmed['pink-4']}'
+"
