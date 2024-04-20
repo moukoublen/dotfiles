55d48ecc0e1ad9f811e7df70a722882ebef90220
Kostas Stamatakis <moukoublen@gmail.com>
2024-04-20 14:32:56 +0300

    cleanup

--- lib/00-homebrew.bash
+++ lib/00-homebrew.bash
@@ -1,28 +1,31 @@
-if [[ "$(uname)" == "Linux" ]]; then
-  if [ ! -d '/home/linuxbrew/.linuxbrew' ]; then
+case "$(uname)" in
+
+  "Darwin")
+    if [[ ! -d '/opt/homebrew' ]]; then
     return 0
-  fi
+    fi
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
+      export BASH_COMPLETION_COMPAT_DIR="${HOMEBREW_PREFIX}/etc/bash_completion.d"
+      source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+    fi
+    ;;
 
-  if [ -d "${HOMEBREW_PREFIX}/etc/bash_completion.d" ]; then
-    for rc in "${HOMEBREW_PREFIX}/etc/bash_completion.d"/*; do
-      if [ -f "${rc}" ]; then
-        source "${rc}"
-      fi
-    done
-  fi
-  unset rc
-fi
+  "Linux")
+    if [ ! -d '/home/linuxbrew/.linuxbrew' ]; then
+      return 0
+    fi
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-if [[ "$(uname)" == "Darwin" ]]; then
-  if [[ ! -d '/opt/homebrew' ]]; then
-    return 0
-  fi
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+    if [ -d "${HOMEBREW_PREFIX}/etc/bash_completion.d" ]; then
+      for rc in "${HOMEBREW_PREFIX}/etc/bash_completion.d"/*; do
+        if [ -f "${rc}" ]; then
+          source "${rc}"
+        fi
+      done
+    fi
+    unset rc
+    ;;
 
-  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
-    export BASH_COMPLETION_COMPAT_DIR="${HOMEBREW_PREFIX}/etc/bash_completion.d"
-    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
-  fi
-fi
+esac
