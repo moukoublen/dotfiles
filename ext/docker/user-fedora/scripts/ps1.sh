677b8e3855249e6cc2b887c80cfb6f815e8fdd80
Kostas Stamatakis <moukoublen@gmail.com>
2024-04-21 10:57:47 +0300

    cleanup

--- ext/docker/user-fedora/scripts/ps1.sh
+++
@@ -1,7 +0,0 @@
-if [[ ! -z $BASH ]]; then
-    if [[ $EUID -eq 0 ]]; then
-        PS1="\[\e[33m\][\[\e[m\]\[\e[31m\]\u \[\e[33m\]\W\[\e[m\]\[\e[33m\]]\[\e[m\] # "
-    else
-        PS1="\[\e[36m\][\[\e[m\]\[\e[34m\]\u \[\e[32m\]\W\[\e[m\]\[\e[36m\]]\[\e[m\] \$ "
-    fi
-fi
--- ext/notes/macos_notes.md
+++
@@ -1,5 +0,0 @@
-# Use BASH from brew as default shell
-
-`sudo bash -c "echo '/usr/local/bin/bash' >> /etc/shells"`
-
-`chsh -s /usr/local/bin/bash`
