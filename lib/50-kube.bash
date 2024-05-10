cf8c38f00dc2b7f88a686052a92ad48db6011ded
Kostas Stamatakis <moukoublen@gmail.com>
2024-05-10 16:41:12 +0300

    updates

--- lib/50-kube.bash
+++ lib/50-kube.bash
@@ -8,20 +8,3 @@ if [[ -x /usr/local/bin/kubectl ]]; then
   alias k=kubectl
   complete -F __start_kubectl k
 fi
-
-#if command -v minikube 1>/dev/null 2>&1; then
-if [[ -x /usr/local/bin/minikube ]]; then
-  source <(minikube completion bash)
-fi
-
-if [[ -x /usr/local/bin/helm ]]; then
-  source <(helm completion bash)
-fi
-
-if [[ -x /usr/local/bin/kind ]]; then
-  source <(kind completion bash)
-fi
-
-if [[ -x /usr/local/bin/k9s ]]; then
-  source <(k9s completion bash)
-fi
