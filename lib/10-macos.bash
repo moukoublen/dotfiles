0afcb3f4fa571ea044d713813c8a37ef1ff904fc
Kostas Stamatakis <kostas.stamatakis@elastic.co>
2026-01-23 13:00:24 +0200

    update macos

--- lib/10-macos.bash
+++ lib/10-macos.bash
@@ -7,21 +7,20 @@ path-add /usr/local/bin/
 
 #export JAVA_HOME='/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home'
 
+# alias code='/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
 alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
-#alias code='/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
-alias cleanup_finder_shit="find . -type f -name '*.DS_Store' -ls -delete"
 alias ldd='otool -L'
-alias tree='command tree -C'
 alias mac-no-quarantine='xattr -r -d com.apple.quarantine'
 
-if [[ -d ${HOMEBREW_PREFIX}/opt/grep/libexec/gnubin ]]; then
+if [[ -d "${HOMEBREW_PREFIX}/opt/grep/libexec/gnubin" ]]; then
+  # path-add "${HOMEBREW_PREFIX}/opt/grep/libexec/gnubin"
   alias grep='command ggrep --color=auto'
 else
   alias grep='command grep --color=auto'
 fi
 
-# PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
-if [[ -d ${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin ]]; then
+if [[ -d "${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin" ]]; then
+  # path-add "${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin"
   alias ll='command gls -la --color=auto'
   alias ls='command gls --color=auto'
   alias cat='command gcat'
@@ -31,19 +30,29 @@ else
   alias ls='command ls -G'
 fi
 
-# brew install findutils (find, locate, updatedb, xargs)
-# [[ -d "${HOMEBREW_PREFIX}/opt/findutils/libexec/gnubin" ]] && path-add "${HOMEBREW_PREFIX}/opt/findutils/libexec/gnubin"
+if [[ -d "${HOMEBREW_PREFIX}/opt/findutils/bin" ]]; then
+  # path-add "${HOMEBREW_PREFIX}/opt/findutils/libexec/gnubin"
+  alias find='command gfind'
+fi
 
 if [[ -d "${HOMEBREW_PREFIX}/opt/make/libexec/gnubin" ]]; then
+  # path-add "${HOMEBREW_PREFIX}/opt/make/libexec/gnubin"
   alias make='command gmake'
 fi
 
 if [[ -d "${HOMEBREW_PREFIX}/opt/gnu-sed/libexec/gnubin" ]]; then
+  # path-add "${HOMEBREW_PREFIX}/opt/gnu-sed/libexec/gnubin"
   alias sed='command gsed'
 fi
 
 if [[ -d "${HOMEBREW_PREFIX}/opt/curl/bin" ]]; then
+  # path-add "${HOMEBREW_PREFIX}/opt/curl/bin"
   alias curl="\${HOMEBREW_PREFIX}/opt/curl/bin/curl"
+fi
+
+if [[ -d "${HOMEBREW_PREFIX}/opt/tree/bin" ]]; then
+  # path-add "${HOMEBREW_PREFIX}/opt/tree/bin"
+  alias tree="\${HOMEBREW_PREFIX}/opt/tree/bin/tree -C"
 fi
 
 export LANG='en_US.UTF-8'
@@ -69,3 +78,7 @@ export EDITOR="${HOMEBREW_PREFIX}/bin/vim"
 
 # brew list installed package content
 # brew ls --verbose <name/s>
+
+cleanup-finder-shit() {
+  find . -type f -name '*.DS_Store' -ls -delete
+}
