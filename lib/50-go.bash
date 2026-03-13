0b879c430b5f84c0f26af0802a2bfb2d8323f645
Kostas Stamatakis <moukoublen@gmail.com>
2026-03-13 23:00:10 +0200

    (go): cleanups

--- lib/50-go.bash
+++ lib/50-go.bash
@@ -1,5 +1,3 @@
-# GOEXPERIMENT=loopvar
-
 if [ -d /usr/local/go ]; then
   # No need of GOROOT in case of default path (/usr/local/go)
   #export GOROOT=/path/to/special/go
@@ -15,12 +13,6 @@ if command -v go 1>/dev/null 2>&1; then
   fi
 
   path-add "$(go env GOPATH)/bin"
-fi
-
-if command -v golangci-lint 1>/dev/null 2>&1; then
-  # https://www.shellcheck.net/wiki/SC1090
-  # shellcheck source=/dev/null
-  source <(golangci-lint completion bash)
 fi
 
 go-test() {
@@ -63,17 +55,6 @@ install-go-tools() { (
   go-install "golang.org/x/tools/cmd/fiximports@${GO_X_TOOLS_VER}"
   go-install "golang.org/x/tools/cmd/goimports@${GO_X_TOOLS_VER}"
   go-install "golang.org/x/tools/cmd/gorename@${GO_X_TOOLS_VER}"
-  go-install "golang.org/x/tools/go/analysis/passes/defers/cmd/defers@${GO_X_TOOLS_VER}"
-  go-install "golang.org/x/tools/go/analysis/passes/fieldalignment/cmd/fieldalignment@${GO_X_TOOLS_VER}"
-  go-install "golang.org/x/tools/go/analysis/passes/findcall/cmd/findcall@${GO_X_TOOLS_VER}"
-  go-install "golang.org/x/tools/go/analysis/passes/ifaceassert/cmd/ifaceassert@${GO_X_TOOLS_VER}"
-  go-install "golang.org/x/tools/go/analysis/passes/lostcancel/cmd/lostcancel@${GO_X_TOOLS_VER}"
-  go-install "golang.org/x/tools/go/analysis/passes/nilness/cmd/nilness@${GO_X_TOOLS_VER}"
-  go-install "golang.org/x/tools/go/analysis/passes/ifaceassert/cmd/ifaceassert@${GO_X_TOOLS_VER}"
-  go-install "golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow@${GO_X_TOOLS_VER}"
-  go-install "golang.org/x/tools/go/analysis/passes/stringintconv/cmd/stringintconv@${GO_X_TOOLS_VER}"
-  go-install "golang.org/x/tools/go/analysis/passes/unmarshal/cmd/unmarshal@${GO_X_TOOLS_VER}"
-  go-install "golang.org/x/tools/go/analysis/passes/unusedresult/cmd/unusedresult@${GO_X_TOOLS_VER}"
   # go vet -vettool=$(which shadow) ./...
 
   go-install "golang.org/x/vuln/cmd/govulncheck@latest"
@@ -88,15 +69,13 @@ install-go-tools() { (
   # https://github.com/golangci/golangci-lint/releases
   #go-install 'github.com/golangci/golangci-lint/cmd/golangci-lint@latest'
   curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)/bin"
+  golangci-lint completion bash > "${HOME}/.bashrc.d/golangci-lint-completion.bash"
 
   # https://github.com/securego/gosec/releases
   go-install 'github.com/securego/gosec/v2/cmd/gosec@latest'
 
   # https://github.com/mvdan/gofumpt/releases
   go-install 'mvdan.cc/gofumpt@latest'
-
-  # https://github.com/daixiang0/gci/releases
-  go-install 'github.com/daixiang0/gci@latest'
 
   # https://github.com/mgechev/revive/releases
   go-install 'github.com/mgechev/revive@latest'
@@ -105,18 +84,9 @@ install-go-tools() { (
   ################################################################
 
   # https://github.com/mozilla/sops/releases
-  go-install 'github.com/getsops/sops/v3/cmd/sops@latest'
-
-  # https://github.com/orijtech/structslop/tags
-  go-install 'github.com/orijtech/structslop/cmd/structslop@latest'
-
-  # https://github.com/jesseduffield/lazydocker/releases
-  #go-install github.com/jesseduffield/lazydocker@latest
+  # go-install 'github.com/getsops/sops/v3/cmd/sops@latest'
 
   go-install github.com/google/gops@latest
-
-  #go-install github.com/elastic/elastic-package@latest
-  #go-install github.com/cashapp/hermit/cmd/hermit@latest
 ); }
 
 # Where is the go ENV file?
