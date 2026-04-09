6ebfba3c2f4121024bb8b2d0fa7bc549762cff87
Kostas Stamatakis <kostas.stamatakis@elastic.co>
2026-04-09 12:15:12 +0300

    update macos lib

--- lib/10-macos.bash
+++ lib/10-macos.bash
@@ -59,6 +59,15 @@ fi
 # export LANG='en_US.UTF-8'
 # export LC_ALL='en_US.UTF-8'
 
+export LANG="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
+export LC_ALL='en_US.UTF-8'
+
 install-nano-nanorc() {
   touch ~/.nanorc
   for rcfile in "$(brew --prefix nano)"/share/nano/*.nanorc; do
